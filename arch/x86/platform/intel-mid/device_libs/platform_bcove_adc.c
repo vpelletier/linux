9514df9e0fd5f2daad2a4aa7f054b315ec52e1a8
Vincent Pelletier <plr.vincent@gmail.com>
2017-03-10 16:01:04 +0000

    WIP

--- arch/x86/platform/intel-mid/device_libs/platform_bcove_adc.c
+++ arch/x86/platform/intel-mid/device_libs/platform_bcove_adc.c
@@ -1,7 +1,7 @@
 /*
- * Platform data for Intel Merrifield Basin Cove GPADC driver
+ * Platform data for Basin Cove GPADC driver
  *
- * (C) Copyright 2012 Intel Corporation
+ * (C) Copyright 2017 Vincent Pelletier <plr.vincent@gmail.com>
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -9,9 +9,9 @@
  * of the License.
  */
 
-#include <linux/sfi.h>
 #include <linux/iio/iio.h>
 #include <linux/iio/machine.h>
+#include <linux/sfi.h>
 
 #include <asm/intel-mid.h>
 #include <asm/intel_scu_ipc.h>
@@ -23,7 +23,7 @@ static struct resource bcove_adc_resources[] = {
 };
 
 static struct platform_device bcove_adc_dev = {
-	.name		= "msic_adc",
+	.name		= "basin_cove_adc",
 	.id		= PLATFORM_DEVID_NONE,
 	.num_resources	= ARRAY_SIZE(bcove_adc_resources),
 	.resource	= bcove_adc_resources,
@@ -50,21 +50,14 @@ static int __init register_bcove_adc(void)
 		return -ENODEV;
 
 	/*
-	 * We need to be sure that the SCU IPC is ready before adc device
+	 * We need to be sure that the SCU IPC is ready before ADC device
 	 * can be registered:
 	 */
 	intel_scu_notifier_add(&bcove_adc_scu_notifier);
 
 	return 0;
 }
-/*
- * On one hand we need to be sure that resources are filled before we
- * proceed with driver registration. On the other hand we need to be
- * sure that SCU IPC driver appears after we filled resources. Since the
- * first is happened at arch_initcall and the last at device_initcall, we
- * choose postcore_initcall for the driver registration.
- */
-postcore_initcall(register_bcove_adc);
+arch_initcall(register_bcove_adc);
 
 static void __init *bcove_adc_platform_data(void *info)
 {
